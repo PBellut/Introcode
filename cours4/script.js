f5437db665b1781653770dc80b266140ab7b45cd
PBellut <36660412+PBellut@users.noreply.github.com>
2018-04-17 20:34:17 +0200

    Cours 4 - Projet final

--- cours4/script.js
+++ cours4/script.js
@@ -3,15 +3,15 @@ $(document).ready(function() {
  });
 function getUsername() {
     var  user = prompt("Veuillez rentrez votre nom:", "");
-    return user
+    return user;
 };
  function welcome() {
     var name= getUsername();
-    $("#welcome").html('<h4>Bienvenue '+name+', <br> soyez pret à renouveler de nouveaux défis ! </h4>');
+    $("#welcome").html('<h4>Bienvenue '+name+', <br> Tiens toi pret à renouveler de nouveaux défis ! </h4>');
 };
 function loadChallenges() {
   $.ajax(
-        {url : 'https://s3.eu-central-1.amazonaws.com/spintransfer/challenge.json',
+        {url : 'https://api.myjson.com/bins/v7rxn',
         type: 'GET',
         dataType: 'json'} 
   )
